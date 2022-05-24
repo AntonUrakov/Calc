30f80c21b9c8877235c887b9ed20311fd554a5a3
AntonUrakov <AntonUrakov@gmail.com>
2022-05-24 21:40:20 +0300

    1

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const exp = document.getElementById('exp');
-const calcButton = document.getElementById('calc');
-const calcResult = document.getElementById('result');
+const exp = document.getElementById("exp");
+const calcButton = document.getElementById("calc");
+const calcResult = document.getElementById("result");
 
-calcButton.addEventListener('click', calcFunc);
+calcButton.addEventListener("click", calcFunc);
 
 const actions = {
   multiplication: {
-    value: '*',
-    label: 'multiplication',
-    func: (a,b) => (parseInt(a) * parseInt(b))
+    value: "*",
+    label: "multiplication",
+    func: (a, b) => parseInt(a) * parseInt(b),
   },
   division: {
-    value: '/',
-    label: 'division',
-    func: (a,b) => (a / b)
+    value: "/",
+    label: "division",
+    func: (a, b) => a / b,
   },
   addition: {
-    value: '+',
-    label: 'addintion',
-    func: (a,b) => (parseInt(a) + parseInt(b))
+    value: "+",
+    label: "addintion",
+    func: (a, b) => parseInt(a) + parseInt(b),
   },
   subtraction: {
-    value: '-',
-    label: 'subtraction',
-    func: (a,b) => (parseInt(a) - parseInt(b))
-  }
-}
+    value: "-",
+    label: "subtraction",
+    func: (a, b) => parseInt(a) - parseInt(b),
+  },
+};
 
 function calcFunc() {
   const res = parseBrackets(exp.value);
@@ -45,7 +45,7 @@ function parseBrackets(str) {
 
 function calcExpr(str) {
   let res;
-  Object.keys(actions).map(function(type) {
+  Object.keys(actions).map(function (type) {
     res = parseExpr(str, actions[type]);
     if (res) {
       str = str.replace(res.str, res.value.toString());
@@ -54,16 +54,16 @@ function calcExpr(str) {
   });
   return str;
 }
- 
+
 function parseExpr(str, action) {
   const reg = new RegExp(`((\\d+)\\s*\\${action.value}\\s*(\\d+))`);
   const out = str.match(reg);
   if (!out) return false;
-  
+
   const result = {
-    str: out[1]
+    str: out[1],
   };
-  
+
   result.value = action.func(out[2], out[3]);
   return result;
 }
